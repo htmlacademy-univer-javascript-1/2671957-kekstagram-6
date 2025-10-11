642eaa7a5e2f5b9b8fc30768ec615efd85f9c6cc
Keks <keks@htmlacademy.ru>
2025-10-11 19:53:41 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/util.js
@@ -0,0 +1,19 @@
+// Возвращает случайное целое число из диапазона [min, max] включительно
+export const getRandomIntInclusive = (min, max) => {
+  const lower = Math.ceil(Math.min(min, max));
+  const upper = Math.floor(Math.max(min, max));
+  return Math.floor(Math.random() * (upper - lower + 1)) + lower;
+};
+
+// Возвращает случайный элемент из переданного массива
+export const getRandomArrayElement = (arr) =>
+  arr[getRandomIntInclusive(0, arr.length - 1)];
+
+// Генератор последовательных уникальных идентификаторов
+export const createIdGenerator = (start = 1) => {
+  let current = start - 1;
+  return () => {
+    current += 1;
+    return current;
+  };
+};
